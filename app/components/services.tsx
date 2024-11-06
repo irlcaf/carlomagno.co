65dd248c3b658d5bdc844a70e984da9259c481b6
Carlomagno Amaya <carlomagnoaf@hotmail.com>
2024-11-06 03:04:00 +0100

    updating summary and description

--- app/components/services.tsx
+++ app/components/services.tsx
@@ -18,14 +18,17 @@ export function Services() {
             className="flex flex-col space-y-1 mb-4"
             href={`/services/${post.slug}`}
           >
-            <div className="w-full flex flex-col md:flex-row space-x-0 md:space-x-2">
-              <p className="text-neutral-600 dark:text-neutral-400 w-[40px] tabular-nums">
+            <div className="w-full flex flex-col md:flex-row items-start space-x-0 md:space-x-4 p-4 bg-white dark:bg-neutral-800 rounded-t-lg shadow-lg border border-neutral-200 dark:border-neutral-700">
+              <span className="text-neutral-600 dark:text-neutral-400 w-[40px] tabular-nums">
                 {post.metadata.icon}
-              </p>
-              <p className="text-neutral-900 dark:text-neutral-100 tracking-tight">
+              </span>
+              <h2 className="text-neutral-900 dark:text-neutral-100 text-lg font-semibold tracking-tight">
                 {post.metadata.title}
-              </p>
+              </h2>
             </div>
+            <p className="text-neutral-600 dark:text-neutral-500 text-sm mt-2 md:mt-0 p-4 bg-neutral-50 dark:bg-neutral-900 rounded-b-lg shadow-sm border-t-0 border-neutral-200 dark:border-neutral-700">
+              {post.metadata.summary}
+            </p>
           </Link>
         ))}
     </div>
